1b6c684d972aac6d22d60f31fc5c596ade0de88e
alexrilodev <alex4rl96@gmail.com>
2025-06-12 23:43:01 +0200

    Update config

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-define("BASE_URL", "/proyecto-it-juniors/");
+define("BASE_URL", "/");
 require_once __DIR__ . '/database/db.php';
 ?>
